59b1b15732bfb941d4c633b41bdb1cdad5a8af01
Bounty556 <jacobmayday@gmail.com>
2020-08-31 10:16:25 -0600

    Setup react router to route to Search and Saved pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,18 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import Search from './pages/Search';
+import Saved from './pages/Saved';
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Search} />
+        <Route exact path='/Saved' component={Saved} />
+      </Switch>
+    </Router>
   );
 }
-
 
 export default App;
---
+++ client/src/pages/Saved/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Saved() {
+  return <div>Saved Page!</div>;
+};
+
+export default Saved;
